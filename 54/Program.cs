45042fa6f18d1428f01977073a426432a9b4dd4b
Akkenton <akkenton.msk@gmail.com>
2023-01-05 17:08:06 +0300

    Решение задачи №54

--- 54/Program.cs
+++ 54/Program.cs
@@ -7,3 +7,55 @@
 // 7 4 2 1  
 // 9 5 3 2  
 // 8 4 4 2  
+
+int[,] array = {                // Зададим матрицу вручную
+                {1,4,7,2},
+                {5,9,2,3},
+                {8,4,2,4},
+                };
+
+void SortMatrix(int[,] array, int row)  // метод сортировки строки в двумерном массиве
+{
+    int temp = 0;
+    bool flag = true;        // флаг нужен для отработки условия, что строка отсортирована
+    while (flag == true)     // сортировка строки в цикле (методом пузырька)
+    {
+        flag = false;
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            if (i < array.GetLength(1) - 1)
+            {
+                if (array[row, i] < array[row, i + 1])
+                {
+                    temp = array[row, i];
+                    array[row, i] = array[row, i + 1];
+                    array[row, i + 1] = temp;
+                    flag = true;
+                }
+            }
+            else break;
+        }
+    }
+}
+
+void PrintArray(int[,] array)   // метод печати массива
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+// --------------------  Тело программы
+
+Console.Clear();
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    SortMatrix(array, i);
+}
+
+PrintArray(array);
